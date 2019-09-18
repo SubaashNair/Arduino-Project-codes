029cb9f6b0624b9532e331b13e10433cb30d3bf8
Subaash Nair <valiban12@gmail.com>
2019-09-18 18:21:43 +0800

    Create LED-on and off with better debouncing.ino
    Added a better debouncing code.

---
+++ LED-on and off with better debouncing.ino
@@ -0,0 +1,31 @@
+```
+const int LED = 13;      // the pin for the LED
+const int BUTTON = 7;   // the input pin where the pushbutton is connected
+int button_val = 0;            // val will be used to store the state of the input pin
+int old_button_val = 0;        // this variable stores the previous value of "button_val"
+int LED_state = 0;          // 0 = LED off and 1 = LED on
+
+void setup() {
+pinMode(LED, OUTPUT); // tell Arduino LED is an output
+pinMode(BUTTON, INPUT); // and BUTTON is an input
+}
+
+void loop(){
+button_val = digitalRead(BUTTON); // read input value and store it
+
+// check if there was a transition
+if ((val == HIGH) && (old_val == LOW)){
+state = 1 - state;
+delay(10);
+}
+old_button_val = button_val; // val is now old, let's store it
+if (LED_state == 1) {
+digitalWrite(LED, HIGH); // turn LED ON
+} else {
+digitalWrite(LED, LOW);
+}
+}
+if ((val == LOW) && (old_val == HIGH)) {
+delay(10);
+}
+```
